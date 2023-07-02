e29a2d51f13c1127b20ce8beba60e2ecab80c0d0
SysSolutions <131492089+SysSolutionsPE@users.noreply.github.com>
2023-07-02 17:00:00 -0500

    Thanks Maxxy

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ import moment from 'moment-timezone'
 import { group } from 'console'
 import PhoneNumber from 'awesome-phonenumber'
 /*============= WAKTU =============*/
-let wibh = moment.tz('Asia/Jakarta').format('HH')
-    let wibm = moment.tz('Asia/Jakarta').format('mm')
-    let wibs = moment.tz('Asia/Jakarta').format('ss')
+let wibh = moment.tz('America/Lima').format('HH')
+    let wibm = moment.tz('America/Lima').format('mm')
+    let wibs = moment.tz('America/Lima').format('ss')
     let wktuwib = `${wibh} H ${wibm} M ${wibs} S`
     
     let d = new Date(new Date + 3600000)
@@ -39,58 +39,58 @@ const readMore = more.repeat(4001)
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ NO OWNER ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 
 global.owner = [
-['6285380166282', 'ᴋᴀʜꜰɪ ᴍᴀᴋᴇʀ✓', true],
-['6285380166282', 'ᴋᴀʜꜰɪ ᴍᴀᴋᴇʀ✓', true]
+['51945463812', 'SysSolutions', true],
+['51966099729', 'SysSolutionsPeru', true]
 ]
-global.mods = []
-global.prems = []
-global.nomorbot = '6285295374964'
-global.nomorown = '6285380166282'
-global.nomorown1 = '6285380166285'
-global.nomorown2 = '6285380166282'
-global.nomorwa = '0'
+global.mods = ['51945463812']
+global.prems = ['51945463812']
+global.nomorbot = '51945463812'
+global.nomorown = '51945463812'
+global.nomorown1 = '51945463812'
+global.nomorown2 = '51945463812'
+global.nomorwa = '51945463812'
 
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ WM ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 
 global.readMore = readMore
-global.author = 'Created By ᴋᴀʜꜰɪ ᴍᴀᴋᴇʀ✓'
-global.namebot = 'ʏᴛ ᴍᴀxxʏ ʙᴏᴛᴢ✓'
-global.wm = '© 𝘾𝙧𝙚𝙖𝙩𝙚𝙙 𝘽𝙮 ᴋᴀʜꜰɪ ᴍᴀᴋᴇʀ✓'
-global.media = 'https://telegra.ph/file/881df749a21bf4c6fbc7d.jpg'
-global.watermark = wm
-global.wm2 = 'ʏᴛ ᴍᴀxxʏ ʙᴏᴛᴢ✓ '
-global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
-global.packname = wm
-global.bottime = `T I M E : ${wktuwib}`
-global.titlebot = `ʏᴛ ᴍᴀxxʏ ʙᴏᴛᴢ✓ 𝐗 ᴋᴀʜꜰɪ ᴍᴀᴋᴇʀ✓`
-global.stickpack = 'ʏᴛ ᴍᴀxxʏ ʙᴏᴛᴢ✓ 𝐗 ᴋᴀʜꜰɪ ᴍᴀᴋᴇʀ✓'
-global.stickauth = `© ʏᴛ ᴍᴀxxʏ ʙᴏᴛᴢ✓ MD`
-global.week = `${week} ${date}`
-global.wibb = `${wktuwib}`
+global.author = 'Created By SysSolutions'
+global.namebot = 'ʏᴛ : SysSolutionsPeru'
+global.wm = '©SysSolutions'
+global.media = 'https://telegra.ph/file/7e93e63346657d87e7f30.jpg'
+global.watermark = 'SysSolutions'
+global.wm2 = 'ʏᴛ :' SysSolutions'
+global.botdate = '⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}'
+global.packname ='SysSolutions' 
+global.bottime = 'T I M E : ${wktuwib}'
+global.titlebot = 'ʏᴛ @SysSolutionsPeru'
+global.stickpack = 'ʏᴛ @SysSolutionsPeru'
+global.stickauth = '©SysSolutionsPeru'
+global.week = '${week} ${date}'
+global.wibb = '${wktuwib}'
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ OWNER INFO ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
-global.nameown1= 'ᴋᴀʜꜰɪ ᴍᴀᴋᴇʀ✓'
-global.nameown2 = 'ʏᴛ ᴍᴀxxʏ ʙᴏᴛᴢ✓'
+global.nameown1= 'SysSolutions'
+global.nameown2 = 'SysSolutionsPeru'
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ MEDIA LINK ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
-global.Linkgc = 'Bentar'
-global.lynk = 'https://lynk.id/kahfi.xd'
+global.Linkgc = 'SysSolutions'
+global.lynk = 'www.syssolutions.tk'
 
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ MEDIA SOSSIAL ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
-global.sig = 'https://instagram.com/kahfiofc0'
-global.sgh = 'https://github.com/Frezzy-XD' //github
+global.sig = 'https://instagram.com/SysSolutionsPeru'
+global.sgh = 'https://github.com/SysSolutionsPE' //github
 global.sgc = 'https://chat.whatsapp.com/I0prmYcZhKrCye0kiclzyt' //group whatsapp
 global.sdc = '-' //discord
-global.snh = 'https://youtube.com/channel/UC6GPl9xMWL61NAXQb3HBrRw'
+global.snh = 'https://chat.whatsapp.com/I78hAmdyLzt8zBIxOIHRtd'
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ DONASI ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
-global.pdana = '0853-8016-6282'
-global.povo = '-'
-global.pgopay = '-'
+global.pdana = '51945463812'
+global.povo = '51945463812'
+global.pgopay = '51945463812'
 global.plinkaja = '-' //link aja
-global.ppulsa = '085295374964'
-global.ppulsa2 = '085295374964'
-global.psaweria = 'https://saweria.co/MaxxyBotz'
+global.ppulsa = '51945463812'
+global.ppulsa2 = '51945463812'
+global.psaweria = 'https://saweria.co/SysSolutions'
 
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ TAMPILAN ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
@@ -263,19 +263,19 @@ global.drtf = 'text/rtf'
 //OPTIONS
 global.optsnsfw = true // Untuk mematikan Dan Menyalakan Nsfw
 global.premnsfw = true // Nsfw Mode Premium?
-global.thumb = 'https://telegra.ph/file/d228cf9e49e7413568a1c.jpg' //Main Thumbnail
-global.imagebot = 'https://telegra.ph/file/d228cf9e49e7413568a1c.jpg'
+global.thumb = 'https://telegra.ph/file/7e93e63346657d87e7f30.jpg' //Main Thumbnail
+global.imagebot = 'https://telegra.ph/file/7e93e63346657d87e7f30.jpg'
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
-global.thumbs = ['https://telegra.ph/file/d228cf9e49e7413568a1c.jpg']
+global.thumbs = ['https://telegra.ph/file/7e93e63346657d87e7f30.jpg']
 global.thumbnailUrl = [
-  'https://telegra.ph/file/81260a8b9e8cff26d2b48.jpg', 'https://telegra.ph/file/ac4928f0824a2a0492737.jpg',
-  'https://telegra.ph/file/6359b013bc7e52c3b346f.jpg', 'https://telegra.ph/file/d43c89a5d2da72875ec05.jpg',
-  'https://telegra.ph/file/7d6c0e35f9c8f52715541.jpg', 'https://telegra.ph/file/ef4b742d47e6a9115e2ff.jpg',
-  'https://telegra.ph/file/55e5af5f33fbd57104187.jpg', 'https://telegra.ph/file/af236598456b95884bd15.jpg',
-  'https://telegra.ph/file/de92ed4a729887ffc974c.jpg', 'https://telegra.ph/file/00ce42a193b1dbbf907d4.jpg'
+  'https://telegra.ph/file/7e93e63346657d87e7f30.jpg', 'https://telegra.ph/file/7e93e63346657d87e7f30.jpg',
+  'https://telegra.ph/file/7e93e63346657d87e7f30.jpg', 'https://telegra.ph/file/7e93e63346657d87e7f30.jpg',
+  'https://telegra.ph/file/7e93e63346657d87e7f30.jpg', 'https://telegra.ph/file/7e93e63346657d87e7f30.jpg',
+  'https://telegra.ph/file/7e93e63346657d87e7f30.jpg', 'https://telegra.ph/file/7e93e63346657d87e7f30.jpg',
+  'https://telegra.ph/file/7e93e63346657d87e7f30.jpg', 'https://telegra.ph/file/7e93e63346657d87e7f30.jpg'
 ]
-global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/e72de2cbba03f58eba7d1.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/7e93e63346657d87e7f30.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/7e93e63346657d87e7f30.jpg' //ini juga ganti 
 global.flaaa2 = [
  "https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=",
  "https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=",
@@ -304,16 +304,16 @@ global.hoppai = ['https://telegra.ph/file/f34d76df4a2065af1a5ba.jpg','https://te
 global.logo = 'https://i.pinimg.com/564x/f7/d2/e4/f7d2e48fd59a8c01cd396bfc70b0a2d1.jpg'
 
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/Frezzy-XD"
+global.urlnya = "https://www.instagram.com/SysSolutionsPeru"
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '085380166282'
+global.phn = '51945463812'
 
-global.thumbdoc = 'https://telegra.ph/file/881df749a21bf4c6fbc7d.jpg'
+global.thumbdoc = 'https://telegra.ph/file/7e93e63346657d87e7f30.jpg'
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ JNGN DI UBAH ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 
 global.fsizedoc = '99999999999999'
 global.fpagedoc = '999'
-global.thumbdoc = 'https://chat.whatsapp.com/I0prmYcZhKrCye0kiclzyt'
+global.thumbdoc = 'https://chat.whatsapp.com/I78hAmdyLzt8zBIxOIHRtd'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
